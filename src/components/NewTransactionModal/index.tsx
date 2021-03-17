b776765d0cf4d281e0da70b04d0737413be50f39
Felipe Augusto Rosa <flp.far@gmail.com>
2021-03-17 08:05:22 -0300

    Add post transaction to miragejs

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 
-import closeImg from '../../assets/close.svg';
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
@@ -11,50 +12,90 @@ interface NewTransactionModalProps {
   onRequestClose: () => void;
 }
 
-export function NewTransmissionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-  const [type, setType] = useState('deposit');
+export function NewTransmissionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  function handleCreatenNewTransaction(e: FormEvent) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      amount,
+      category,
+      type,
+    }
+
+    api.post('transactions', data)
+
+    onRequestClose();
+  }
 
   return (
     <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        overlayClassName="react-modal-overlay"
-        className="react-modal-content"
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
       >
-        <button type="button" onClick={onRequestClose} className="react-modal-close">
-          <img src={closeImg} alt="Fechar modal"/>
-        </button>
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
 
-        <Container>
-          <h2>Cadastrar transação</h2>
+      <Container onSubmit={handleCreatenNewTransaction}>
+        <h2>Cadastrar transação</h2>
 
-          <input type="text" placeholder="Título" />
-          <input type="number" placeholder="Valor" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
 
-          <TransactionTypeContainer>
-            <RadioBox 
-              type="button"
-              onClick={() => setType('deposit')}
-              isActive={type === 'deposit'}
-              activeColor="green"
-            >
-              <img src={incomeImg} alt="Entrada"/>
-              <span>Entrada</span>
-            </RadioBox>
-            <RadioBox 
-              type="button"
-              onClick={() => setType('withdraw')}
-              isActive={type === 'withdraw'}
-              activeColor="red"
-            >
-              <img src={outcomeImg} alt="Saída"/>
-              <span>Saída</span>
-            </RadioBox>
-          </TransactionTypeContainer>
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
 
-          <input type="text" placeholder="Categoria" />
-          <button type="submit">Cadastrar</button>
-        </Container>
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
-  )
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { createServer } from 'miragejs';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
   routes() {
-    this.namespace = 'api';
+    this.namespace = "api";
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        }
-      ]
-    })
-  }
-})
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
